7484c819d33260dcb48c3d2f223718c20eaa1da5
Your Name <you@example.com>
2021-12-31 22:57:53 +0700

    no message

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server= http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://hardcore-stonebraker-b391ff.netlify.app",
         methods:["GET","POST"],
     }
 });
